0d98d4cae83c671f8f5ac0987b7c0a9002d2221e
Yash Gupta <ygupta96@gmail.com>
2019-07-03 16:19:21 -0400

    Fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,47 +1,12 @@
 import React from 'react';
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile
-} from "react-device-detect";
-
 import { compose } from 'recompose';
-// @material-ui/core components
 import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
-import Button from '@material-ui/core/Button'
-// import BarcodeReader from 'react-barcode-reader'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography'
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsconfig from '../../aws-exports';
 
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import MaterialTable from "material-table";
-import { Radio, RadioGroup, TextField } from '@material-ui/core';
-// const { protobuf } = require('sawtooth-sdk');
-// const { createHash } = require('crypto')
+Amplify.configure(awsconfig);
 
-
+console.log("working!");
 
 
 class HomePage extends React.Component {
