1f0d8655591f3bc498fec5d6560f4f94a2d4d6d5
Justin Unterreiner <j@unterr.com>
2019-07-10 20:06:08 -0400

    Added getPotentialMatches function as a stub + Timestamp in getFreeTimes output changed from string to int

--- amplify/backend/function/getFreeTimes/src/index.js
+++ amplify/backend/function/getFreeTimes/src/index.js
@@ -11,15 +11,15 @@ exports.handler = function (event, context) { //eslint-disable-line
 				[
 					{ 
 						'id': 'f677d95f-c42e-4f35-9abf-67f171a5230g',
-						'timestamp': '1562797800'
+						'timestamp': 1562797800
 					},
 					{ 
 						'id': 'f677d95f-c42e-4f35-9abf-67f171a52b53',
-						'timestamp': '1562799600'
+						'timestamp': 1562799600
 					},
 					{ 
 						'id': 'f677d95f-c42e-4f35-9abf-67f171a52d42',
-						'timestamp': '1562803200'
+						'timestamp': 1562803200
 					}
 				]
 			};
---
+++ amplify/backend/function/getPotentialMatches/src/index.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+
+exports.handler = function (event, context) { //eslint-disable-line
+
+	if(!event.hasOwnProperty('id')){
+
+		throw new Error('Missing value for id');
+	}
+
+	stub = { 'potentialMatches':
+				[
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-67f171a5230f',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-67f171a52b53',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-67f171a52d42',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-6bd042f02ab4',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-91c94d32feb3',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-6948de8f20ab',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-98840def6gab',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					},
+					{ 
+						'id': 'f677d95f-c42e-4f35-9abf-04892fg73eb3',
+						'url': 'http://www.aussiehomepcrepair.com/wp-content/uploads/2014/05/User-No-Frame.png'
+					}
+				]
+			};
+
+	context.done(null, stub);
+};
