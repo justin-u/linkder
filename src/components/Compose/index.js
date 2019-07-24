3a810ef768039d928c6e0deeeb08ec500f8b55c5
Adit Kumar <aditkuman@gmail.com>
2019-07-24 16:36:17 -0400

    compose

--- src/components/Compose/index.js
+++ src/components/Compose/index.js
@@ -1,68 +1,62 @@
 import React, { Component } from 'react';
+import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
 import './Compose.css';
+import { compose } from 'recompose';
+import { withFirebase } from '../Firebase';
 
-export default class Compose extends Component {
-  constructor(props) {
-    super(props)
+class Compose extends Component {
+  constructor(props) {
+    super(props)
 
-    const authUser = JSON.parse(localStorage.getItem('authUser'));
+    const authUser = JSON.parse(localStorage.getItem('authUser'));
 
-    this.state = {
-      authUser: authUser,
-      inputValue: ''
-    }
-  }
+    this.state = {
+      authUser: authUser,
+      inputValue: ''
+    }
+  }
 
-  onSendMessage = (content, type) => {
-    if (content.trim() === '') {
-      return
-    }
+  onSendMessage = () => {
+    console.log(this.state.inputValue);
+    this.props.firebase.messages(this.state.authUser.uid, "D7q4mJiJaYgjlgJWXtv0gh1csJB2").push({
+      text: this.state.inputValue,
+      author: this.state.authUser.uid,
+      time: new Date().getTime()
+    })
+  }
 
-    // const timestamp = moment()
-    //   .valueOf()
-    //   .toString()
+  onKeyboardPress = event => {
+    if (event.key === 'Enter') {
+      this.onSendMessage()
+    }
+  }
 
-    const itemMessage = {
-      idFrom: this.authUser,
-      idTo: this.currentPeerUser.id,
-      content: content.trim(),
-      type: type
-    }
+  render() {
+    return (
+      <div className="compose">
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={event => {
+            this.setState({ inputValue: event.target.value })
+          }}
+          className="compose-input"
+          placeholder={"Type a message, " + this.state.authUser.name.substring(0, this.state.authUser.name.indexOf(' '))}
+          onKeyPress={this.onKeyboardPress}
+        />
 
-    // myFirestore
-    //   .collection(AppString.NODE_MESSAGES)
-    //   .doc(this.groupChatId)
-    //   .collection(this.groupChatId)
-    //   .set(itemMessage)
-    //   .then(() => {
-    //     this.setState({ inputValue: '' })
-    //   })
-  }
-
-  onKeyboardPress = event => {
-    if (event.key === 'Enter') {
-      this.onSendMessage(this.state.inputValue, 0)
-    }
-  }
-
-  render() {
-    return (
-      <div className="compose">
-        <input
-          type="text"
-          value={this.state.inputValue}
-          onChange={event => {
-            this.setState({ inputValue: event.target.value })
-          }}
-          className="compose-input"
-          placeholder={"Type a message, " + this.state.authUser.name.substring(0, this.state.authUser.name.indexOf(' ')) }
-          onKeyPress={this.onKeyboardPress}
-        />
-
-        {
-          this.props.rightItems
-        }
-      </div>
-    );
-  }
+        {
+          this.props.rightItems
+        }
+      </div>
+    );
+  }
 }
+
+const condition = authUser => !!authUser;
+
+export default compose(
+  withFirebase,
+  withEmailVerification,
+  withAuthorization(condition),
+  )(Compose)
